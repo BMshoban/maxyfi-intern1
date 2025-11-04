55b5d6fe7da5d73c32da964339971d2369e39a53
devopss projects practice <shoban200327@gmail.com>
2025-11-04 14:35:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        ...............Finally DEPLOYED by SHOBAN ................
+        ...............Finally DEPLOYED by SHOBAN devops intern................
       </p>
     </>
   )
